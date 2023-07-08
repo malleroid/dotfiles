9558af6566328df677050fe23b08b4a3c19c1001
malleroid <malleus@malleus.me>
2023-07-08 20:00:00 +0900

    :gear: add shell symbolic link generation

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DOTFILES_DIR=$(cd $(dirname $0); pwd)
+
+ln -snfv $DOTFILES_DIR/gitui ~/.config
+ln -snfv $DOTFILES_DIR/fish ~/.config
+ln -snfv $DOTFILES_DIR/.gitconfig ~/.gitconfig
+ln -snfv $DOTFILES_DIR/.gitignore ~/.gitignore
+ln -snfv $DOTFILES_DIR/.commit_template ~/.commit_template
