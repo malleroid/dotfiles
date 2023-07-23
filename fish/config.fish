67617500a9222052499433c54cfab0cd04759ff7
malleroid <malleus@malleus.me>
2023-07-23 20:00:00 +0900

    :gear: add rbenv initial process

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,7 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source (nodenv init -|psub)
+    source (rbenv init -|psub)
 end
 
 # fish_vi_key_bindings
