5cacd7f473b6e31bbcf2e40e990e7bed1dcc0c2e
malleroid <malleus@malleus.me>
2023-07-27 02:03:24 +0900

    :recycle: refact shellcheck

--- link.sh
+++ link.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-DOTFILES_DIR=$(cd $(dirname $0); pwd)
+DOTFILES_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 
-ln -snfv $DOTFILES_DIR/gitui ~/.config
-ln -snfv $DOTFILES_DIR/fish ~/.config
-ln -snfv $DOTFILES_DIR/.gitconfig ~/.gitconfig
-ln -snfv $DOTFILES_DIR/.gitignore ~/.gitignore
-ln -snfv $DOTFILES_DIR/.commit_template ~/.commit_template
+ln -snfv "$DOTFILES_DIR"/gitui ~/.config
+ln -snfv "$DOTFILES_DIR"/fish ~/.config
+ln -snfv "$DOTFILES_DIR"/.gitconfig ~/.gitconfig
+ln -snfv "$DOTFILES_DIR"/.gitignore ~/.gitignore
+ln -snfv "$DOTFILES_DIR"/.commit_template ~/.commit_template
