b531ec8f744992d4885f33042754441c39095637
malleroid <malleus@malleus.me>
2023-07-28 18:22:38 +0900

    :gear: add nvim config file

--- link.sh
+++ link.sh
@@ -4,6 +4,7 @@ DOTFILES_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 
 ln -snfv "$DOTFILES_DIR"/gitui ~/.config
 ln -snfv "$DOTFILES_DIR"/fish ~/.config
+ln -snfv "$DOTFILES_DIR"/nvim ~/.config
 ln -snfv "$DOTFILES_DIR"/.gitconfig ~/.gitconfig
 ln -snfv "$DOTFILES_DIR"/.gitignore ~/.gitignore
 ln -snfv "$DOTFILES_DIR"/.commit_template ~/.commit_template
