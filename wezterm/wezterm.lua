644cadba8b3f3b10743fb1c0c52475e1ccf0b580
malleroid <malleus@malleus.me>
2024-04-15 02:08:01 +0900

    :gear: update wezterm config to define local value

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,9 @@
-return {
-	color_scheme = "iceberg-dark",
-	window_background_opacity = 0.80,
-	scrollback_lines = 10000,
-}
+local wezterm = require 'wezterm';
+
+local config = {};
+
+config.color_scheme = "iceberg-dark";
+config.window_background_opacity = 0.80;
+config.scrollback_lines = 10000;
+
+return config;
