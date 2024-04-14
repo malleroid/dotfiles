d5570934939557f25fdc824b9db6332cb3b0de21
malleroid <malleus@malleus.me>
2024-04-15 03:09:14 +0900

    :gear: output keyconfig & load config file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,4 +6,8 @@ config.color_scheme = "iceberg-dark";
 config.window_background_opacity = 0.80;
 config.scrollback_lines = 35000;
 
+-- keys
+config.keys = require("keybinds").keys;
+config.key_tables = require("keybinds").key_tables;
+
 return config;
