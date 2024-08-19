502927a88c2760b2fddb2ea301555820d3c69093
malleroid <malleus@malleus.me>
2024-08-20 00:37:24 +0900

    :up: add gh-cli extension gh-dash install process

--- setup.sh
+++ setup.sh
@@ -18,3 +18,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # fish setup
 fish -c "curl -sL git.io/fisher | source && fisher update"
+
+# gh extension
+fish -c "gh extension install dlvhdr/gh-dash"
