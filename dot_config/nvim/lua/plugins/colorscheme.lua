05035d9bd399601bce2831724ffe9ae2bf2ca95c
malleroid <38337195+malleroid@users.noreply.github.com>
2026-03-22 22:24:20 +0900

    :art: enable transparent background for tokyonight

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -10,20 +10,6 @@ return {
         sidebars = "transparent",
         floats = "transparent",
       },
-      on_colors = function(colors)
-        -- Align with Ghostty Cyberdyne palette
-        colors.bg = "NONE"
-        colors.bg_dark = "NONE"
-        colors.bg_sidebar = "NONE"
-        colors.bg_float = "NONE"
-        colors.green = "#00c172"      -- Cyberdyne green
-        colors.green1 = "#00ff92"     -- Cyberdyne foreground green
-        colors.cyan = "#6bffdd"       -- Cyberdyne cyan
-        colors.red = "#ff8373"        -- Cyberdyne red
-        colors.yellow = "#d2a700"     -- Cyberdyne yellow
-        colors.blue = "#0071cf"       -- Cyberdyne blue
-        colors.magenta = "#ff90fe"    -- Cyberdyne magenta
-      end,
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
