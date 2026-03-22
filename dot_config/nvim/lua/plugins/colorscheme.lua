c978603aad23cc2e02d101985b6ffdb562c5d5ac
malleroid <38337195+malleroid@users.noreply.github.com>
2026-03-23 01:01:52 +0900

    :art: switch to cyberdream.nvim with Cyberdyne palette

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,31 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      style = "night",
       transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+      colors = {
+        -- Ghostty Cyberdyne palette
+        bg = "#151144",
+        bg_alt = "#080808",
+        bg_highlight = "#454d96",
+        fg = "#00ff92",
+        grey = "#2e2e2e",
+        blue = "#0071cf",
+        green = "#00c172",
+        cyan = "#6bffdd",
+        red = "#ff8373",
+        yellow = "#d2a700",
+        magenta = "#ff90fe",
+        pink = "#ffb2fe",
+        orange = "#ffc4be",
+        purple = "#e6e7fe",
       },
     },
     config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd([[colorscheme tokyonight-night]])
+      require("cyberdream").setup(opts)
+      vim.cmd([[colorscheme cyberdream]])
     end,
   },
 }
