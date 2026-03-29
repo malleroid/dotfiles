60d2bc412cb9513cda9d1506572a8f15c8f8bbc6
malleroid <38337195+malleroid@users.noreply.github.com>
2026-03-29 22:57:59 +0900

    :art: switch colorscheme from cyberdream to emerald-synth

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,10 @@
 return {
   {
-    "scottmckendry/cyberdream.nvim",
+    "malleroid/emerald-synth.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      transparent = true,
-      colors = {
-        -- Ghostty Cyberdyne palette
-        bg = "#151144",
-        bg_alt = "#080808",
-        bg_highlight = "#454d96",
-        fg = "#00ff92",
-        grey = "#2e2e2e",
-        blue = "#0071cf",
-        green = "#00c172",
-        cyan = "#6bffdd",
-        red = "#ff8373",
-        yellow = "#d2a700",
-        magenta = "#ff90fe",
-        pink = "#ffb2fe",
-        orange = "#ffc4be",
-        purple = "#e6e7fe",
-      },
-    },
-    config = function(_, opts)
-      require("cyberdream").setup(opts)
-      vim.cmd([[colorscheme cyberdream]])
+    config = function()
+      vim.cmd([[colorscheme emerald-synth]])
     end,
   },
 }
