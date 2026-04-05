24914633129e798b8641af0df0946386c71ec457
malleroid <38337195+malleroid@users.noreply.github.com>
2026-04-05 18:36:14 +0900

    :art: enable transparent background for nvim emerald-synth

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("emerald-synth").setup({ transparent = true })
       vim.cmd([[colorscheme emerald-synth]])
     end,
   },
