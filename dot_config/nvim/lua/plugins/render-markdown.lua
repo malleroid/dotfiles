5e12052c1c3dfa60d07ab87b552faa97377c3ea8
malleroid <38337195+malleroid@users.noreply.github.com>
2026-04-12 21:50:56 +0900

    :heavy_plus_sign: add render-markdown.nvim for in-buffer markdown preview

---
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("render-markdown").setup({})
+    end,
+  },
+}
