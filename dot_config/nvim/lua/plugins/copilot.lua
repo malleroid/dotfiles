8f013e819cd19c640824142218b4b43db75f5a92
malleroid <38337195+malleroid@users.noreply.github.com>
2026-04-13 00:26:14 +0900

    :recycle: switch Copilot from raw lspconfig to copilot.lua
    Neovim core lacks textDocument/inlineCompletion support, so use copilot.lua for proper inline suggestions with blink.cmp integration

---
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = "<M-k>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<M-h>",
+          },
+        },
+        panel = { enabled = false },
+      })
+
+      -- Hide copilot suggestions when blink.cmp menu is open
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BlinkCmpMenuOpen",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BlinkCmpMenuClose",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = false
+        end,
+      })
+    end,
+  },
+}
